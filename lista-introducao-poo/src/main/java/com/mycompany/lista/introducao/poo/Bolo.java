b8dde7186157aae8b93285dfef0fbd1663d706b0
BRyuTakahashi <bruno.takahashi@sptech.school>
2023-03-30 17:10:29 -0300

    alterando a função comprarBolo

--- lista-introducao-poo/src/main/java/com/mycompany/lista/introducao/poo/Bolo.java
+++ lista-introducao-poo/src/main/java/com/mycompany/lista/introducao/poo/Bolo.java
@@ -6,10 +6,9 @@ public class Bolo {
     Integer quantidadeVendida;
     
     void comprarBolo(Integer quantidadeComprada){
-        if(quantidadeVendida > 100){
+        
+        if((quantidadeVendida + quantidadeComprada) > 100){
             System.out.println("Seu pedido ultrapassou nosso limite diário para esse bolo");
-        } else if(quantidadeVendida + quantidadeComprada > 100){
-            System.out.println("A quantidade desejada para compra excede o limite diário");
         } else {
             quantidadeVendida += quantidadeComprada;
             System.out.println(String.format("Você adicionou mais %d bolos de %s, totalizando %d bolos", quantidadeComprada, sabor,quantidadeVendida));
